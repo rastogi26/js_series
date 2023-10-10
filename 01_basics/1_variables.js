8de4098ac979ebb7726f3c07a71bc739d8c0fef2
Dhruv Rastogi <72301136+rastogi26@users.noreply.github.com>
2023-10-10 05:43:25 +0000

    variables done

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12233
+let accountEmail ="dhruv@google.com"
+var accountPassword = "23767"
+accountCity ="solan"
+
+accountEmail ="123@gmail.com"
+accountPassword="12334"
+accountCity="shimala"
+
+console.table([accountEmail,accountId,accountCity])
---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; 
+
+alert(3+3) // error because we using nodejs not browser
+
+//use mdn
+
