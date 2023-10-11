dd69a0635d70f33c948c01c98a7578e11568977f
Dhruv Rastogi <72301136+rastogi26@users.noreply.github.com>
2023-10-11 12:38:31 +0000

    functions_done

--- 03_basics/2_scopes.js
+++ 03_basics/2_scopes.js
@@ -10,3 +10,38 @@ if(true)
 // console.log(a);err
 // console.log(b);err
 // console.log(c);  //run but problem 
+
+//nested scopes
+
+function one() {
+    const name ="dhruv"
+
+    function two() {
+        const website = "youtube"
+        console.log(name);
+    }
+
+    // console.log(website); err
+    two()
+}
+
+one()
+
+//**************intersesting */
+
+
+//method1
+
+console.log(addOne(5))
+function addOne(num) {
+    return num+1
+}
+
+//method 2
+//hosting
+// console.log(addTwo(9));  //gives err
+const addTwo = function(num) {
+    return num+2
+}
+
+console.log(addTwo(9));
