ef78be6f6e988f2d274e625474e8cab655e81206
Dhruv Rastogi <72301136+rastogi26@users.noreply.github.com>
2023-10-12 07:43:55 +0000

    arrow_done

--- 03_basics/3_arrow.js
+++ 03_basics/3_arrow.js
@@ -5,8 +5,48 @@ const user ={
     price: 999,
     welcomeMessage: function(){
         console.log(`${this.username}, Welcome to website`)
+        // console.log(this);
     }
 }
 
 
-user.welcomeMessage()
+// user.welcomeMessage()
+user.username="sam"
+// user.welcomeMessage()
+
+// console.log(this);  // current context = {}empty
+
+// function chai()
+// {   
+//     let username ="dhruv"
+//     console.log(this.username);
+// }
+
+
+// const chai = function(){
+//     let user ="dhruv"
+//     console.log(this.user);
+// }
+
+// chai()
+
+
+const chai = ()=>{
+    let username ="dhruv"
+    console.log(username);
+}
+
+// chai()
+
+// const addTwo = (num1,num2)=>{
+//     return num1+num2
+// }
+
+//implicit return
+// const addTwo =  (num1,num2) =>(num1+num2)
+const addTwo = (num1,num2) => num1+num2
+console.log(addTwo(2,4));
+const objectReturn = () =>({username:"dhruv"})
+console.log(objectReturn());
+
+
